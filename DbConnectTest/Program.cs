4389b53c5e8b60f1b3858275a1a6fa302d0af57d
Paul Lee <ppauleee@gmail.com>
2017-10-31 17:13:51 -0700

    Add project files.

---
+++ DbConnectTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbConnectTest.Domain;
+using DbConnectTest.Service;
+
+namespace DbConnectTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductService pservice = new ProductService();
+
+            IEnumerable<Product> prod = pservice.GetAllProducts();
+
+            foreach (Product product in prod)
+            {
+                Console.WriteLine(
+                    $"Product name: {product.ProductName} - product description: {product.ProductDescription}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
